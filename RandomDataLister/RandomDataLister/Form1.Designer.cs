669ab6255b69bf7eeea4ed5d3535cd85916b3f49
vaidotas.but@gmail.com <vaidotas.but@gmail.com>
2020-02-17 14:58:43 +0200

    Added .sln, form

---
+++ RandomDataLister/RandomDataLister/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace RandomDataLister
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.ThreadNo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.RandomRow = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ThreadNo,
+            this.RandomRow});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(-1, 44);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(934, 383);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // ThreadNo
+            // 
+            this.ThreadNo.Text = "Thread No";
+            // 
+            // RandomRow
+            // 
+            this.RandomRow.Text = "RandomRow";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkGreen;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(12, 446);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 26);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AccessibleDescription = "";
+            this.textBox1.Location = new System.Drawing.Point(286, 448);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(45, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(163, 448);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Thread No [2-15]";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 484);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Data List";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader ThreadNo;
+        private System.Windows.Forms.ColumnHeader RandomRow;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ RandomDataLister/RandomDataLister/Form1.cs
@@ -0,0 +1,141 @@
+﻿using RandomDataLister.Database;
+using RandomDataLister.DataGenerator;
+using RandomDataLister.Model;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomDataLister
+{
+    public partial class Form1 : Form
+    {
+        bool isWorking;
+        Thread writeThr;
+        BlockingCollection<RandomData> producedData = new BlockingCollection<RandomData>(boundedCapacity: 30);
+        List<Thread> threads = new List<Thread>();
+        Query query = new Query();
+        public Form1()
+        {
+            InitializeComponent();
+            listView1.View = View.Details;
+            listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!isWorking)
+            {
+                int count = 0;
+                if (int.TryParse(textBox1.Text, out count) && count >= 2 && count <= 15)
+                {
+                    listView1.Items.Clear();
+                    button1.Text = "STOP";
+                    button1.BackColor = Color.DarkRed;
+                    isWorking = true;
+                    label1.ForeColor = Color.Black;
+
+                    query.CreateConn();
+                    writeThr = new Thread(Write);
+                    writeThr.Start();
+                    threads.Clear();
+                    GenerateRandomString();
+                }
+                else
+                {
+                    label1.ForeColor = Color.DarkRed;
+                }
+            }
+            else
+            {
+                button1.Text = "START";
+                button1.BackColor = Color.DarkGreen;
+                isWorking = false;
+
+                try
+                {
+                    query.CloseConn();
+                    threads.ForEach(t => t.Abort());
+                    writeThr.Abort();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+        }
+        private void GenerateRandomString()
+        {
+            try
+            {
+                int threadCount = int.Parse(textBox1.Text);
+                if (threadCount >= 2 && threadCount <= 15)
+                {
+                    Generator generator = new Generator(ref producedData, threadCount);
+                    for (int i = 1; i <= threadCount; i++)
+                    {
+                        int j = i;
+                        Thread thr = new Thread(() => generator.Generate(j));
+                        thr.Start();
+                        threads.Add(thr);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void Write()
+        {
+            while (isWorking)
+            {
+                if (producedData.Count == 0)
+                {
+                    Thread.Sleep(10);
+                    continue;
+                }
+
+                RandomData rnd = producedData.Take();
+                ListViewItem item = new ListViewItem(new[] { rnd.ThreadId.ToString(), rnd.Data });
+
+                if (listView1.InvokeRequired)
+                {
+                    listView1.Invoke((MethodInvoker)delegate ()
+                    {
+                        listView1.Items.Add(item);
+                        listView1.EnsureVisible(listView1.Items.Count - 1);
+
+                        int count = listView1.Items.Count;
+                        if (count > 20)
+                            listView1.Items.Remove(listView1.Items[0]);
+                    });
+                }
+
+                query.Write(rnd);
+            }
+        }
+    }
+}
+
