62f75e67db1289806b1c21949688431a64ebb9cb
vaidotas.but@gmail.com <vaidotas.but@gmail.com>
2020-02-17 14:42:37 +0200

    Add database, model, query.cs

---
+++ RandomDataLister/RandomDataLister/Database/Query.cs
@@ -0,0 +1,37 @@
+﻿using RandomDataLister.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomDataLister.Database
+{
+    public class Query
+    {
+        SqlConnection conn = new SqlConnection();
+
+        public void CreateConn()
+        {
+            conn.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;
+                                    AttachDbFilename=C:\Users\Vaidas\Desktop\DEV\RandomDataLister\RandomDataLister\RandomDataLister\Database\Database1.mdf;
+                                    Integrated Security=True;
+                                    pooling=true";
+            conn.Open();
+        }
+        public void CloseConn()
+        {
+            conn.Close();
+        }
+
+        public void Write(RandomData data)
+        {
+            string query = $@"INSERT RandomData (ThreadId, Data, Time) VALUES ('{data.ThreadId}', '{data.Data}', '{data.Time}')";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.ExecuteNonQuery();
+        }
+
+    }
+}
